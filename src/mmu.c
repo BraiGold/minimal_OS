933c0b2d875ae24c9cbb2b5da4032edb1e77bd69
Maximiliano Leon Paz <m4xileon@gmail.com>
2015-10-29 19:46:38 -0300

    intento de ej 4 .

--- src/mmu.c
+++ src/mmu.c
@@ -20,23 +20,61 @@ uint ind_free_page = PDE_DESC;
 /* Direcciones fisicas de directorios y tablas de paginas del KERNEL */
 /* -------------------------------------------------------------------------- */
 
+void mmu_copiar_pagina(uint src, uint dst){
+	uint i = 0;
+	while(i < 1024){
+		*((uint *)(dst + i*4)) = *((uint *)(src + i*4));;
+		i++;
+
+	}
+}
+
 // crea el directorio, las paginas, copia el codigo e inicializa el stack
 uint mmu_inicializar_memoria_perro(perro_t *perro, int index_jugador, int index_tipo){
-	uint dir_tarea;
+	uint cr3 = mmu_proxima_pagina_fisica_libre_directory() ;
+	mmu_inicializar_pagina(cr3);
+
 	if (index_jugador == 0){
+		perro->x = 0;
+		perro->y = 0;
 		if (index_tipo == 0){
-			dir_tarea= 0x10000;
+			dir_perro= 0x10000;
 		}else{
-			dir_tarea= 0x11000;
+			dir_perro= 0x11000;
 		}
 	}else{
+		perro->x = 78;
+		perro->y = 34;
 		if (index_tipo == 0){
-			dir_tarea= 0x12000;	
+			dir_perro= 0x12000;	
 		}else{
-			dir_tarea= 0x13000;
+			dir_perro= 0x13000;
 		}
 	}
-	return dir_tarea;
+	// copio en la primera pagina el codigo del perro
+	// uint dir_perro_area_libre = mmu_proxima_pagina_fisica_libre_directory() ;
+	// mmu_inicializar_pagina( dir_perro_area_libre);
+	 
+	mmu_copiar_pagina(dir_perro, dir_perro_area_libre);
+	// seteo propiedades del perro
+	perro->tipo = index_tipo;
+	perro->libre = false;
+	perro->huesos = 0;
+
+	// Mapeo el codigo del perro a la posicion de la cucha cucha
+	uint ind_page_table  = 0;
+	uint dir_lineal = MAPA_BASE_VIRTUAL;
+	uint dir_fisica = MAPA_BASE_FISICA;
+	while(ind_page_table < 1024){
+		// En la dir lineal no pongo la pagina porque la va a pedir cuando mapee
+		dir_fisica += 4;
+		dir_lineal += 4;
+		mmu_mapear_pagina(dir_lineal , cr3 , dir_fisica, 0x3);
+		ind_page_table++;
+	}
+	
+
+	return cr3;
 	
 
 }
@@ -68,7 +106,10 @@ uint mmu_inicializar_dir_kernel(){
 	// identity mapping
 
 	mmu_identity_mapping(0,cr3);
-		
+
+	// inicializo el contador de paginas libres en el ox1000
+	ind_free_page = MAPA_BASE_FISICA_LIBRE;
+
 	return cr3 ;
 
 }
@@ -103,7 +144,7 @@ void mmu_mapear_pagina  (uint virtual, uint cr3, uint fisica, uint attrs){
 }
 
 void mmu_unmapear_pagina(uint virtual, uint cr3){
-	breakpoint();
+	//breakpoint();
 	uint directory_11_0 = virtual;
 	directory_11_0 = (directory_11_0 >> 22) * 4;
 	uint* directory = (uint*)(cr3 + (directory_11_0));
